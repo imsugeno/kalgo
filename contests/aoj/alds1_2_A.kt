ffe01b7c470695177c3943ab73d7abe193ee363f
imsugeno <g.tokyo.kazusa@gmail.com>
2025-12-01 00:05:17 +0900

    add

--- contests/aoj/alds1_2_A.kt
+++ contests/aoj/alds1_2_A.kt
@@ -1,13 +1,10 @@
-package aoj
-
 fun main() {
     val n = readln().toInt()
     val nums = readln().split(" ").map { it.toInt() }.toMutableList()
-
     var hasNext = true
     while (hasNext) {
         hasNext = false
-        for (i in n downTo 1) {
+        for (i in n-1 downTo 1) {
             if (nums[i] < nums[i - 1]) {
                 val temp = nums[i]
                 nums[i] = nums[i - 1]
@@ -16,4 +13,5 @@ fun main() {
             }
         }
     }
+    println(nums)
 }
