4bb87f0e78c1a0e2fe58cdaf59a4f16b30d8aec5
imsugeno <g.tokyo.kazusa@gmail.com>
2025-12-04 08:53:58 +0900

    add

--- contests/aoj/alds1_2_A.kt
+++ contests/aoj/alds1_2_A.kt
@@ -1,17 +1,35 @@
+//fun main() {
+//    val n = readln().toInt()
+//    val nums = readln().split(" ").map { it.toInt() }.toMutableList()
+//    var hasNext = true
+//    while (hasNext) {
+//        hasNext = false
+//        for (i in n-1 downTo 1) {
+//            if (nums[i] < nums[i - 1]) {
+//                val temp = nums[i]
+//                nums[i] = nums[i - 1]
+//                nums[i - 1] = temp
+//                hasNext = true
+//            }
+//        }
+//    }
+//    println(nums)
+//}
+
 fun main() {
     val n = readln().toInt()
     val nums = readln().split(" ").map { it.toInt() }.toMutableList()
-    var hasNext = true
-    while (hasNext) {
-        hasNext = false
-        for (i in n-1 downTo 1) {
+
+    var swapped: Boolean
+    do {
+        swapped = false
+        for (i in n - 1 downTo 1) {
             if (nums[i] < nums[i - 1]) {
-                val temp = nums[i]
-                nums[i] = nums[i - 1]
-                nums[i - 1] = temp
-                hasNext = true
+                nums[i] = nums[i - 1].also { nums[i - 1] = nums[i] }
+                swapped = true
             }
         }
-    }
+    } while (swapped)
+
     println(nums)
 }
